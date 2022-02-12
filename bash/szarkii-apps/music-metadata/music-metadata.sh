9dcfd7350f36df79a05f68d4fe46abbb04b0b855
szarkii <szarkii@protonmail.com>
2022-02-12 16:48:25 +0100

    Add name tag to music-metadata
    * Add possibility to remove metadata - probably don't work

--- bash/szarkii-apps/music-metadata/music-metadata.sh
+++ bash/szarkii-apps/music-metadata/music-metadata.sh
@@ -1,57 +1,14 @@
 #!/bin/bash
 
-VERSION="0.1.0"
+VERSION="0.2.0"
 IFS=$'\n'
 
 function printHelp() {
     echo "Sets metadata for music files. Downloads the file if URL is provided."
     echo "Dependencies: kid3-cli"
-    echo "$(basename $0) [-t track] [-a artist] [-l album] [-y year] [-g genre] [-u URL] input"
+    echo "$(basename $0) [-t track] [-n name] [-a artist] [-l album] [-y year] [-g genre] [-u URL] input"
     echo "  input  file or directory (N/A for -u)"
     exit
-}
-
-# TODO
-function downloadFile() {
-    youtube-dl --prefer-ffmpeg --format "bestaudio/best" --extract-audio --audio-quality 0 --audio-format mp3 --output "$DIR/$name.%(ext)s" "$url"
-}
-
-function setMetadata() {
-    track="$1"
-    artist="$2"
-    album="$3"
-    year="$4"
-    genre="$5"
-    filePath="$6"
-    fileName="$(basename "$filePath")"
-
-    if [[ ! -z "$track" ]]; then
-        echo "Setting \"$track\" track number in $fileName"
-        kid3-cli -c "set track '$track'" "$filePath"
-    fi
-    
-    if [[ ! -z "$artist" ]]; then
-        echo "Setting \"$artist\" artist in $fileName"
-        kid3-cli -c "set artist '$artist'" "$filePath"
-    fi
-    
-    if [[ ! -z "$album" ]]; then
-        echo "Setting \"$album\" album in $fileName"
-        kid3-cli -c "set album '$album'" "$filePath"
-    fi
-
-    if [[ ! -z "$year" ]]; then
-        echo "Setting $year year in $fileName"
-        kid3-cli -c "set date '$year'" "$filePath"
-    fi
-
-    if [[ ! -z "$genre" ]]; then
-        echo "Setting $genre genre in $fileName"
-        kid3-cli -c "set genre '$genre'" "$filePath"
-    fi
-
-    id3v2 --list "$filePath"
-    echo
 }
 
 if [[ "$#" -eq 1 && ("$1" = "-h" || "$1" = "--help") ]]; then
@@ -68,14 +25,72 @@ if [[ "$#" -lt 3 ]]; then
     printHelp
 fi
 
-while getopts t:a:l:y:g:u: option; do
+# TODO
+function downloadFile() {
+    youtube-dl --prefer-ffmpeg --format "bestaudio/best" --extract-audio --audio-quality 0 --audio-format mp3 --output "$DIR/$name.%(ext)s" "$url"
+}
+
+function setMetadata() {
+    track="$1"
+    name="$2"
+    artist="$3"
+    album="$4"
+    year="$5"
+    genre="$6"
+    filePath="$7"
+    
+    setMetadataIfNeeded "track" "$track" "$filePath"
+    setMetadataIfNeeded "title" "$name" "$filePath"
+    setMetadataIfNeeded "artist" "$artist" "$filePath"
+    setMetadataIfNeeded "album" "$album" "$filePath"
+    setMetadataIfNeeded "date" "$year" "$filePath"
+    setMetadataIfNeeded "genre" "$genre" "$filePath"
+
+    kid3-cli -c "get" "$filePath"
+    echo
+}
+
+function setMetadataIfNeeded() {
+    metadataName="$1"
+    metadataValue="$2"
+    filePath="$3"
+    fileName="$(basename "$filePath")"
+    
+    if [[ ! -z "$metadataValue" ]]; then
+        echo "Setting $metadataValue $metadataName in $fileName"
+        kid3-cli -c "set $metadataName '$metadataValue'" "$filePath"
+    fi
+}
+
+function deleteMetadata() {
+    metadataToDelete=("$@")
+    ((lastIndex=${#metadataToDelete[@]} - 1))
+    filePath=${metadataToDelete[lastIndex]}
+    unset metadataToDelete[lastIndex]
+
+    for metadata in "${metadataToDelete[@]}"; do
+        kid3-cli -c "remove $metadata" "$filePath"
+    done
+}
+
+metadataToDelete=()
+
+while getopts t:n:a:l:y:g:u:d: option; do
     case "${option}" in
         t) track=${OPTARG} ;;
+        n) name=${OPTARG} ;;
         a) artist=${OPTARG} ;;
         l) album=${OPTARG} ;;
         y) year=${OPTARG} ;;
         g) genre=${OPTARG} ;;
         u) url=${OPTARG} ;;
+        d)  echo "[WARN] Deleting metadata could not work properly."
+            if [[ ${OPTARG} = "year" ]]; then
+                metadataToDelete+=("date")
+            else
+                metadataToDelete+=(${OPTARG})
+            fi
+            ;;
     esac
 done
 
@@ -85,8 +100,10 @@ input="$(realpath "$input")"
 if [[ -d "$input" ]]; then
     for fileName in $(ls "$input"); do
         filePath="$input/$fileName"
-        setMetadata "$track" "$artist" "$album" "$year" "$genre" "$filePath"
+        deleteMetadata "${metadataToDelete[@]}" "$filePath"
+        setMetadata "$track" "$name" "$artist" "$album" "$year" "$genre" "$filePath"
     done
 else
-    setMetadata "$track" "$artist" "$album" "$year" "$genre" "$input"
+    deleteMetadata "${metadataToDelete[@]}" "$input"
+    setMetadata "$track" "$name" "$artist" "$album" "$year" "$genre" "$input"
 fi
